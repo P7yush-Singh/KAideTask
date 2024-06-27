37928e017aa3fa9ed5251c94e45133f1f6365b25
P7yush-Singh <142720296+P7yush-Singh@users.noreply.github.com>
2024-06-27 16:12:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -55,3 +55,32 @@ ScrollReveal().reveal(".A-h1,.Acordino, .A-sc, .F, .S", { delay:2, scale: 2, res
   z: 0
 } } );
 // Image Slider 
+$(".step").click( function() {
+	$(this).addClass("active").prevAll().addClass("active");
+	$(this).nextAll().removeClass("active");
+});
+
+$(".step01").click( function() {
+	$("#line-progress").css("width", "3%");
+	$(".discovery").addClass("active").siblings().removeClass("active");
+});
+
+$(".step02").click( function() {
+	$("#line-progress").css("width", "25%");
+	$(".strategy").addClass("active").siblings().removeClass("active");
+});
+
+$(".step03").click( function() {
+	$("#line-progress").css("width", "50%");
+	$(".creative").addClass("active").siblings().removeClass("active");
+});
+
+$(".step04").click( function() {
+	$("#line-progress").css("width", "75%");
+	$(".production").addClass("active").siblings().removeClass("active");
+});
+
+$(".step05").click( function() {
+	$("#line-progress").css("width", "100%");
+	$(".analysis").addClass("active").siblings().removeClass("active");
+});
